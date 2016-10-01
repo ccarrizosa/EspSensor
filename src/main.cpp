34d3bdd974b81dc24e533c8f8f664c755bcf4cf8
ccarrizosa <c.carrizosa@gmx.com>
2016-10-01 03:19:38 +0200

    Homie framework used to manage WiFi and MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,7 @@
 /*
  * Includes:
  */
-#include <FS.h>
-
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-#include <WiFiManager.h>
-#include <ArduinoJson.h>
+#include <Homie.h>
 #include <Adafruit_ADS1015.h>
 
 #include "Arduino.h"
@@ -23,61 +18,32 @@
  */
 
 #ifdef DBG
-  #define DBG_PRINT(x)  Serial.println (x)
+  #define PRINT(x)  Serial.println (x)
 #else
-  #define DBG_PRINT(x)
+  #define PRINT(x)
 #endif
-
 
 /**
  * How many time (uSec.) the ESP is going to be in deep sleep mode previously
  * to send data again.
  */
-static const uint32_t LOOP_TIME = 300000000;
+static const uint32_t LOOP_TIME = 300000000u;
 
-/**
- * How many time (uSec.) wait after an error
- */
-static const uint32_t RETRY_TIME = LOOP_TIME / 5;
+/** If set to HIGH, the ESP configuration is reset. */
+static const uint8_t RESET_PIN = 14u;
 
+/** While the ADC measurement is ongoing this GPIO is set to high */
+static const uint8_t MEASUREMENT_ONGOING_PIN = 12u;
 
-/*
- * Local vars:
- */
+/** ADC instance */
+Adafruit_ADS1115 ads(0x48);
 
-/**
- * MQTT default values.
- * If there are different values in config.json, they are overwritten.
- */
-char mqtt_server[20];
-char mqtt_user[20];
-char mqtt_password[20];
-char mqtt_port[6]= "1883";
-char mqtt_topic[20];
-
-/** flag for saving data */
-bool shouldSaveConfig = false;
-
-/** client to send MQTT data */
-WiFiClient espClient;
-PubSubClient client(espClient);
-
+/** MQTT node where ADC data is going to be published. */
+HomieNode adcNode("adc", "adc");
 
 /*
  * Functions declaration:
  */
-
-/** callback notifying us of the need to save config */
-void saveConfigCallback ();
-
-/** mqtt callback function */
-void mqtt_callback(char* topic, byte* payload, unsigned int length);
-
-/** Try to enter deep sleep mode. If not, try to reset ESP. */
-void go_to_sleep(uint32_t sleep_time);
-
-/** Send ADS data via MQTT */
-void send_ads_data();
 
 /** Arduino Framework setup function */
 void setup();
@@ -85,223 +51,89 @@ void setup();
 /** Arduino Framework loop function */
 void loop();
 
+/** Homie Events callback function */
+void onHomieEvent(HomieEvent event);
+
+/** Reset configuration condition */
+bool resetFunction();
+
+/** Setup function called once WiFi and MQTT has been connected */
+void setupHandler();
+
+/** Loop function called once WiFi and MQTT has been connected */
+void loopHandler();
+
 /*
  * Functions definition:
  */
 
-void saveConfigCallback () {
-  DBG_PRINT("Should save config");
-  shouldSaveConfig = true;
+bool resetFunction(){
+    return digitalRead(RESET_PIN) == HIGH;
 }
 
-void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+void setupHandler() {
+  ads.begin();
+  pinMode(MEASUREMENT_ONGOING_PIN, OUTPUT);
+}
 
+void loopHandler(){
+  digitalWrite(MEASUREMENT_ONGOING_PIN, HIGH);
+  for(uint8_t channel=0; channel<4; channel++){
+    String value = String(ads.readADC_SingleEnded(channel));
+    PRINT("Channel " + String(channel) + " value = " + value);
+    Homie.setNodeProperty(adcNode, "channel_"+String(channel)).send(value);
+  }
+  digitalWrite(MEASUREMENT_ONGOING_PIN, LOW);
 }
 
 void setup() {
   #ifdef DBG
     Serial.begin(115200);
+  #else
+    Homie.disableLogging();
   #endif
 
-  DBG_PRINT("\n ESP ID: " + String(ESP.getChipId()));
+  Homie_setFirmware("EspSensor", "1.0.1");
 
-  //read configuration from FS json
-  DBG_PRINT("mounting FS...");
+  pinMode(RESET_PIN, INPUT);
+  Homie.disableResetTrigger();
+  Homie.setResetFunction(resetFunction);
+  Homie.disableLedFeedback();
+  Homie.onEvent(onHomieEvent);
+  Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
 
-  if (SPIFFS.begin()) {
-    DBG_PRINT("mounted file system");
-    if (SPIFFS.exists("/config.json")) {
-      //file exists, reading and loading
-      DBG_PRINT("reading config file...");
-      File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile) {
-        DBG_PRINT("Stored Config:");
-        size_t size = configFile.size();
-        // Allocate a buffer to store contents of the file.
-        std::unique_ptr<char[]> buf(new char[size]);
+  adcNode.advertise("channel_0");
+  adcNode.advertise("channel_1");
+  adcNode.advertise("channel_2");
+  adcNode.advertise("channel_3");
+  Homie.setup();
+}
 
-        configFile.readBytes(buf.get(), size);
-        DynamicJsonBuffer jsonBuffer;
-        JsonObject& json = jsonBuffer.parseObject(buf.get());
-
-        #ifdef DBG
-          json.prettyPrintTo(Serial);
-        #endif
-
-        if (json.success()) {
-          DBG_PRINT("\nParsed json");
-
-          strcpy(mqtt_server, json["mqtt_server"]);
-          strcpy(mqtt_user, json["mqtt_user"]);
-          strcpy(mqtt_password, json["mqtt_password"]);
-          strcpy(mqtt_port, json["mqtt_port"]);
-          strcpy(mqtt_topic, json["mqtt_topic"]);
-
-        } else {
-          DBG_PRINT("ERROR: Failed to load json config");
-        }
+void onHomieEvent(HomieEvent event) {
+  switch(event) {
+    case HomieEvent::WIFI_DISCONNECTED:
+    case HomieEvent::MQTT_DISCONNECTED: {
+      static uint8_t retries = 5;
+      if(!retries--){
+        PRINT("Could not connect to WiFi or MQTT server");
+        PRINT("Going to sleep in order to save power.");
+        ESP.deepSleep(LOOP_TIME);
       }
+      break;
     }
-  } else {
-    DBG_PRINT("failed to mount FS");
-    go_to_sleep(RETRY_TIME);
-  }
-  //end read
-
-  //WiFiManager
-  WiFiManager wifiManager;
-
-  #ifndef DBG
-    /* Disable debug output */
-    wifiManager.setDebugOutput(false);
-  #endif
-
-  //wifiManager.resetSettings();
-
-  //set config save notify callback
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
-
-  // MQTT Parameters:
-  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 20);
-  WiFiManagerParameter custom_mqtt_user("user", "mqtt user", mqtt_user, 20);
-  WiFiManagerParameter custom_mqtt_password("password", "mqtt password", mqtt_password, 20);
-  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
-  WiFiManagerParameter custom_mqtt_topic("topic", "mqtt topic", mqtt_topic, 20);
-
-  wifiManager.addParameter(&custom_mqtt_server);
-  wifiManager.addParameter(&custom_mqtt_user);
-  wifiManager.addParameter(&custom_mqtt_password);
-  wifiManager.addParameter(&custom_mqtt_port);
-  wifiManager.addParameter(&custom_mqtt_topic);
-
-  //sets timeout until configuration portal gets turned off
-  wifiManager.setConfigPortalTimeout(300);
-
-  //fetches ssid and pass and tries to connect
-  //if it does not connect it starts an access point with the specified name
-  //and goes into a blocking loop awaiting configuration
-  if (!wifiManager.autoConnect("AutoConnectAP", "password")) {
-    //Failed to connect. Go to deep sleep
-    DBG_PRINT("failed to connect and hit timeout");
-    go_to_sleep(RETRY_TIME);
-  }
-
-  //if you get here you have connected to the WiFi
-  DBG_PRINT("connected...yeey :)");
-
-  //read updated parameters
-  strcpy(mqtt_server, custom_mqtt_server.getValue());
-  strcpy(mqtt_user, custom_mqtt_user.getValue());
-  strcpy(mqtt_password, custom_mqtt_password.getValue());
-  strcpy(mqtt_port, custom_mqtt_port.getValue());
-  strcpy(mqtt_topic, custom_mqtt_topic.getValue());
-
-  DBG_PRINT("MQTT Server: " + String(mqtt_server));
-  DBG_PRINT("MQTT User: " + String(mqtt_user));
-  DBG_PRINT("MQTT Password: " + String(mqtt_password));
-  DBG_PRINT("MQTT Port: " + String(mqtt_port));
-  DBG_PRINT("MQTT Topic: " + String(mqtt_topic));
-
-  //save the custom parameters to FS
-  if (shouldSaveConfig) {
-    DBG_PRINT("saving config");
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& json = jsonBuffer.createObject();
-    json["mqtt_server"] = mqtt_server;
-    json["mqtt_user"] = mqtt_user;
-    json["mqtt_password"] = mqtt_password;
-    json["mqtt_port"] = mqtt_port;
-    json["mqtt_topic"] = mqtt_topic;
-
-    File configFile = SPIFFS.open("/config.json", "w");
-    if (!configFile) {
-      DBG_PRINT("failed to open config file for writing");
-      go_to_sleep(RETRY_TIME);
+    case HomieEvent::MQTT_CONNECTED: {
+      PRINT("MQTT connected, preparing for deep sleep...");
+      Homie.prepareForSleep();
+      break;
     }
-    json.printTo(configFile);
-    configFile.close();
-    //end save
-  }
-
-  DBG_PRINT("ESP ip: ");
-  DBG_PRINT(WiFi.localIP());
-
-  // Setup MQTT client
-  delay(1000);
-  client.setServer(mqtt_server, String(mqtt_port).toInt());
-  client.setCallback(mqtt_callback);
-
-}
-
-void go_to_sleep(uint32_t sleep_time){
-  DBG_PRINT("Going to sleep for " + String(sleep_time) + " uSec.");
-  ESP.deepSleep(sleep_time);
-  delay(1000);
-  ESP.reset();
-}
-
-void reconnect() {
-  uint8_t retries = 5;
-  // Loop until we're reconnected
-  while (!client.connected() && retries--) {
-    DBG_PRINT("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(ESP.getChipId());
-    // Attempt to connect
-    if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
-      DBG_PRINT("connected!");
-    } else {
-      DBG_PRINT("failed, rc=" + String(client.state()));
-      DBG_PRINT(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+    case HomieEvent::READY_FOR_SLEEP: {
+      Serial.println("Ready for sleep");
+      ESP.deepSleep(LOOP_TIME);
+      break;
     }
   }
 }
 
-void send_ads_data(void){
-  Adafruit_ADS1115 ads(0x48);
-  int16_t adc_values[4];
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-
-  ads.begin();
-
-  for(uint8_t channel=0; channel<4; channel++){
-    adc_values[channel] = ads.readADC_SingleEnded(channel);
-
-    root["channel_" + String(channel)] = (String)adc_values[channel];
-    DBG_PRINT("Channel " + String(channel) + " value = " +\
-                   String(adc_values[channel]));
-  }
-
-  #ifdef DBG
-    root.prettyPrintTo(Serial);
-    DBG_PRINT("");
-  #endif
-
-  reconnect();
-  if(!client.connected()){
-    DBG_PRINT("Not possible to connect to MQTT server.");
-    go_to_sleep(RETRY_TIME);
-  }
-
-  client.loop();
-
-  char data[200];
-  root.printTo(data, root.measureLength() + 1);
-  client.publish(mqtt_topic, data, true);
-  DBG_PRINT("Transmission done!");
-
-  delay(1000);
-
-}
-
-/**
- * Main loop
- */
 void loop() {
-  send_ads_data();
-  go_to_sleep(LOOP_TIME);
+  Homie.loop();
 }
